ac801653473b52510fa240f7b3f5b6ec5a46ad27
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2023-05-25 14:25:04 +0300

    fractal stairs made in good position

--- scripts.js
+++ scripts.js
@@ -2,18 +2,18 @@ function setup() {
   createCanvas(600, 500);
 }
 function draw() {
-  angle = PI / 8;
-  translate(width / 2, height);
-  treeBranch(180, 5);
+  angle = PI / 14;
+  translate(width / 1.5, height / 2);
+  treeBranch(180, 1);
 }
 function treeBranch(length, x) {
   noFill();
   rect(0, 0, -length, -length);
-  translate(x, -length);
+  translate(-length, x);
   if (length > 10) {
     push();
     rotate(-angle);
-    treeBranch(length * 0.75);
+    treeBranch(length * 0.96);
     pop();
   }
 }
