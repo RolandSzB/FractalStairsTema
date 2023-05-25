24329a319e764376542f7c3fbb368ab188474b1b
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2023-05-25 14:18:58 +0300

    fractal stairs initialized

--- scripts.js
+++ scripts.js
@@ -2,7 +2,18 @@ function setup() {
   createCanvas(600, 500);
 }
 function draw() {
-  angle = PI;
+  angle = PI / 8;
   translate(width / 2, height);
-  treeBranch(140);
+  treeBranch(180, 5);
+}
+function treeBranch(length, x) {
+  noFill();
+  rect(0, 0, -length, -length);
+  translate(x, -length);
+  if (length > 10) {
+    push();
+    rotate(-angle);
+    treeBranch(length * 0.75);
+    pop();
+  }
 }
